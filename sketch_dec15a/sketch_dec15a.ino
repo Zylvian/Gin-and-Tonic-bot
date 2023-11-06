0548ee68f41ce1c421f9dc1af270e47f42fcb9a2
Jarle Wallevik <jovlisen@gmail.com>
2023-11-06 21:00:16 +0100

    fixed issue, added interrupt

--- sketch_dec15a/sketch_dec15a.ino
+++ sketch_dec15a/sketch_dec15a.ino
@@ -8,19 +8,20 @@ int tonicPinIn2 = 11;
 int pourButtonPin = 7;
 int loadButtonPin = 5;
 
-// Time it takes for liquid to move from start to end of tube.
-int pump_cycle_time = 20000;
+// MEASURES
+// All measures have been made with 4mm tubing connected:
+// Pump to flask tube: 60cm
+/// Pump to glass tube: 30cm
 
+// Time it takes for liquid to move from start to end of tube.
+long pump_cycle_time = 96000;
 
 // Amount of gin and tonic to be dispensed (in cl).
-int gin_amount = 20;
-//int tonic_amount = 80;
-
-// Test amount
-int tonic_amount = 10;
+unsigned long gin_amount = 1;
+unsigned long tonic_amount = 1;
 
 // Time it takes to dispense a centiliter
-int cl_time = 8500;
+unsigned long cl_time = 10500;
 
 // Time when button was pressed;
 unsigned long buttonPressedTime = 0;
@@ -55,7 +56,6 @@ void loop() {
   if(isPouring == false && (millis() - timeStopped > 1000)) {
       // Check if button is pressed.
         if(pourButtonState == LOW){
-          Serial.println("GT pouring started!");
           buttonPressed();
           startPourGT();
         }
@@ -79,11 +79,12 @@ void loop() {
 
 
   // Button dysfunctional atm
-  /*uint8_t loadButtonState = digitalRead(loadButtonPin);
+  uint8_t loadButtonState = digitalRead(loadButtonPin);
   if(loadButtonState == LOW){
-    buttonPressed();
-    loadDrink();
-  }*/
+    //buttonPressed();
+    //loadDrink();
+    Serial.println("this button works?");
+  }
   
   delay(1);
 }
@@ -95,10 +96,19 @@ void loadDrink(){
 }
 
 void startPourGT(){
+  
+  Serial.println("GT pouring started!");
   isPouring = true;
   controlPumps(3, true);
   pump1remaining = gin_amount*cl_time;
   pump2remaining = tonic_amount*cl_time;
+
+
+  Serial.println("---");
+  Serial.println(tonic_amount);
+  Serial.println(cl_time);
+  Serial.println(pump2remaining);
+  Serial.println("---");
 
   Serial.println(pump1remaining);
   Serial.println(timeSinceButton);
@@ -107,8 +117,13 @@ void startPourGT(){
 void continuePour(){
   // Pump 1 check
   bool stopPump1 = pump1remaining <= timeSinceButton;
-  bool stopPump2 = pump2remaining <= timeSinceButton
-  if(stoPump1){
+  bool stopPump2 = pump2remaining <= timeSinceButton;
+
+  Serial.print("PUMP 2: -> ");
+  Serial.println(pump2remaining);
+  Serial.print("TIME SINCE BUTTON: -> ");
+  Serial.println(timeSinceButton, DEC);
+  if(stopPump1){
     controlPumps(1, false);
   }
   if(stopPump2){
@@ -148,8 +163,8 @@ void controlPumps(int which, bool on){
 
 
 void togglePump(int pinIn1, int pinIn2, bool on){
-    Serial.print("A pump was toggled:");
-    Serial.println(on, DEC); 
+    //Serial.print("A pump was toggled:");
+    //Serial.println(on, DEC); 
     digitalWrite(pinIn1, on);
     digitalWrite(pinIn2, LOW);
 }
